01adeb554b62bea938b8801013c85916dd376057
陈栋 <cd2001zxm@126.com>
2018-08-15 15:06:40 +0800

    创建

---
+++ template/nuxt.config.js
@@ -0,0 +1,95 @@
+const path = require('path')
+const vuxLoader = require('vux-loader')
+
+
+module.exports = {
+  analyze:true,
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'teldmobile',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [
+    'vux/src/styles/reset.less',
+    'vux/src/styles/1px.less'
+  ],
+  plugins: [
+    {
+      src: 'teld-vue-m/plugins/vux-plugins',
+      ssr: false
+    },
+    {
+      src: 'teld-vue-m/plugins/vux-components',
+      ssr: true
+    },
+    // {
+    //   src: '~/plugins/vux-components-nossr',
+    //   ssr: false
+    // }
+  ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    filenames: {
+      vendor: 'vendor.js',
+      app: 'app.js',
+      manifest:'manifest.js'
+    },
+    uglify:false,
+    /*
+    ** Run ESLint on save
+    */
+    // extend (config, { isDev, isClient }) {
+    //   if (isDev && isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
+
+    extend(config, { isDev, isClient }) {
+
+
+
+      const configs = vuxLoader.merge(config, {
+        options: {
+          ssr: true
+        },
+        plugins: ['vux-ui', {
+          name: 'less-theme',
+          path: 'teld-vue-m/styles/theme.less'
+        }]
+      })
+
+
+
+      return configs
+    }
+  },
+
+  modules: [
+    //'@nuxtjs/axios',
+  ],
+
+  axios: {
+    // proxyHeaders: false
+  }
+}
